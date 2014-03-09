36f47393b68bd8d5f5f0062bc0e7daed7e93f22f
mwlos <kentro_molos@hotmail.com>
2014-03-09 10:41:54 +0100

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MwlakiApp::Application.routes.draw do
+  get "pages/home"
+  get "pages/contact"
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
