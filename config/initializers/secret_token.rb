779844aa2185ccb591073f6b79b1ba53abdb7f7a
mwlos <kentro_molos@hotmail.com>
2014-03-09 10:20:50 +0100

    100commited

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MwlakiApp::Application.config.secret_key_base = '04e4f69873681f2172734184556c1252ca34ab1f22a5e08523e9a29de009818d1c0c1467ea5ae40b8731eedf97c35c3aa3ee540fc442fe3a4f52395ace5aecb3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MwlakiApp::Application.config.session_store :cookie_store, key: '_mwlaki_app_session'
